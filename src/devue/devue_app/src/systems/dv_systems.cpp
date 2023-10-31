bbeab4472566c3ecad1c131e100123c020163164
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-10-31 23:21:45 +0100

    Bundle app systems

---
+++ src/devue/devue_app/src/systems/dv_systems.cpp
@@ -0,0 +1,5 @@
+#include "systems/dv_systems.hpp"
+
+using namespace devue;
+
+dv_systems::dv_systems() : core::dv_systems_bundle() {}
---
+++ src/devue/devue_app/src/systems/dv_systems.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "systems/dv_systems_bundle.hpp"
+
+namespace devue {
+	struct dv_systems : core::dv_systems_bundle {
+		dv_systems();
+	};
+}
