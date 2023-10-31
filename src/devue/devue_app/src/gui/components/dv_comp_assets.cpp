2b2d876634d8025e74431be46470f6ec90e08a39
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-01 00:06:58 +0100

    Update assets component
    - list imported models

--- src/devue/devue_app/src/gui/components/dv_comp_assets.cpp
+++ src/devue/devue_app/src/gui/components/dv_comp_assets.cpp
@@ -1,5 +1,6 @@
 #include "gui/components/dv_comp_assets.hpp"
 #include "gui/components/dv_components.hpp"
+#include "utilities/dv_util_string.hpp"
 
 using namespace devue;
 using namespace devue::core;
@@ -10,27 +11,17 @@ dv_comp_assets::dv_comp_assets(dv_systems* systems, dv_components* components)
 void dv_comp_assets::render() {
 	ImGui::Begin("Assets##Window");
 
-	/*for (auto& kvp : m_model_system.models) {
-		ds_model& model = kvp.second;
+	for (auto& kvp : m_systems->model.models) {
+		dv_model& model = kvp.second;
 
-		ImGui::PushID(model.guid.to_string().c_str());
+		ImGui::PushID(DV_FORMAT_C("{}", model.uuid));
 
-		if (ImGui::TreeNodeEx(FORMAT_C("{}##Asset", model.name), ImGuiTreeNodeFlags_Leaf)) {
-
-			if (ImGui::BeginPopupContextItem()) {
-				if (ImGui::Selectable("Add to scene##ModelPopup", false,
-					ImGuiSelectableFlags_SelectOnRelease | ImGuiSelectableFlags_NoSetKeyOwner)) {
-					m_scene.add_to_scene(model);
-				}
-
-				ImGui::EndPopup();
-			}
-
-			ImGui::TreePop();
+		if (ImGui::Selectable(DV_FORMAT_C("{}##Asset", model.name), m_components->current_model == model.uuid)) {
+			m_components->current_model = model.uuid;
 		}
 
 		ImGui::PopID();
-	}*/
+	}
 
 	ImGui::End();
 }
