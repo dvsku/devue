ba7251574e86457475b87f07d3ee74fed653d05a
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-10-31 08:02:13 +0100

    Add UUID utility

---
+++ include/devue/devue_core/utilities/dv_util_uuid.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstdint>
+#include <string>
+
+namespace devue {
+	typedef uint64_t uuid;
+}
+
+namespace devue::core {
+	struct dv_util_uuid {
+        static uuid create();
+        static uuid create(const std::string& str);
+	};
+}
