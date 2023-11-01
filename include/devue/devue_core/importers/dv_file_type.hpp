5b56ce11b82f4cf7aca2e8927d44e68f98e16be1
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-01 08:07:47 +0100

    Add file filter ctor

--- include/devue/devue_core/importers/dv_file_type.hpp
+++ include/devue/devue_core/importers/dv_file_type.hpp
@@ -13,6 +13,7 @@ namespace devue::core {
 		std::wstring extensions = L"";
 
 		dv_file_filter() = default;
+		dv_file_filter(std::wstring _name, std::wstring _ext) : name(_name), extensions(_ext) {}
 		dv_file_filter(const dv_file_type& file_type);
 	};
 }
