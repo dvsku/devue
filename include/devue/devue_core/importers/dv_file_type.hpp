2b5ac58f6018455fdec6a476cd45d5587296bfb3
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-01 07:37:54 +0100

    Add structs representing a file type

---
+++ include/devue/devue_core/importers/dv_file_type.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <string>
+
+namespace devue::core {
+	struct dv_file_type {
+		std::string name		= "";
+		std::string extensions	= "";
+	};
+
+	struct dv_file_filter {
+		std::wstring name		= L"";
+		std::wstring extensions = L"";
+
+		dv_file_filter() = default;
+		dv_file_filter(const dv_file_type& file_type);
+	};
+}
