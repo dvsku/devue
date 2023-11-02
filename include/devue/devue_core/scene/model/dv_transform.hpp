ffaa9c55a206d7dadd8774417bff824903af5ad8
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-02 02:12:57 +0100

    Implement dv_transform

--- include/devue/devue_core/scene/model/dv_transform.hpp
+++ include/devue/devue_core/scene/model/dv_transform.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
+#include "glm/glm.hpp"
+
 namespace devue::core {
 	struct dv_transform {
-
+		glm::vec3 position = { 0.0f, 0.0f, 0.0f };
+		glm::vec3 rotation = { 0.0f, 0.0f, 0.0f };
+		glm::vec3 scale    = { 1.0f, 1.0f, 1.0f };
 	};
 }
