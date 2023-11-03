0705221ab67ce827436de3e2ece6d940c544bd72
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-03 07:17:06 +0100

    Show scene models in hierarchy component

--- src/devue_app/src/gui/components/dv_comp_hierarchy.cpp
+++ src/devue_app/src/gui/components/dv_comp_hierarchy.cpp
@@ -1,7 +1,9 @@
 #include "gui/components/dv_comp_hierarchy.hpp"
 #include "gui/components/dv_components.hpp"
+#include "utilities/dv_util_string.hpp"
 
 using namespace devue;
+using namespace devue::core;
 
 dv_comp_hierarchy::dv_comp_hierarchy(dv_systems* systems, dv_components* components) 
 	: dv_comp(systems, components) {}
@@ -25,7 +27,6 @@ void dv_comp_hierarchy::render() {
 
 	ImGui::SetNextItemOpen(true, ImGuiCond_Once);
 	if (ImGui::CollapsingHeader("Cameras##Cameras")) {
-
 		ImGui::Indent();
 
 		if (ImGui::Selectable("Camera 1##Camera1", m_components->selected.in_selected(scene->camera))) {
@@ -33,24 +34,10 @@ void dv_comp_hierarchy::render() {
 		}
 
 		ImGui::Unindent();
-
-		/*ImGui::Indent();
-
-		ImGui::Text("Position");
-
-		ImGui::SetCursorPosX(ImGui::GetCursorPosX() - 3);
-		ImGui::SetNextItemWidth(ImGui::GetContentRegionMax().x);
-		ImGui::BeginDisabled();
-		ImGui::InputFloat3("##SceneExplorerCameraPos", (float*)&m_scene.camera.position, "%.3f", ImGuiInputTextFlags_ReadOnly);
-		ImGui::EndDisabled();
-
-		ImGui::Unindent();*/
 	}
 
 	ImGui::SetNextItemOpen(true, ImGuiCond_Once);
 	if (ImGui::CollapsingHeader("Lighting##Lighting")) {
-
-
 		ImGui::Indent();
 
 		if (ImGui::Selectable("Ambient Light##AmbientLight1", 
@@ -66,105 +53,29 @@ void dv_comp_hierarchy::render() {
 		}
 
 		ImGui::Unindent();
-
-		/*ImGui::SetCursorPosX(ImGui::GetCursorPosX() - 3);
-		ImGui::Checkbox("##SceneExplorerAmbientVisible", &m_scene.lighting.ambient_light.is_active);
-
-		ImGui::SameLine();
-
-		if (ImGui::CollapsingHeader("Ambient light##SceneExplorerAmbientLighting")) {
-			ImGui::Indent(35.0f);
-
-			if (!m_scene.lighting.ambient_light.is_active)
-				ImGui::BeginDisabled();
-
-			m_scene.lighting.ambient_light.build_gui();
-
-			if (!m_scene.lighting.ambient_light.is_active)
-				ImGui::EndDisabled();
-
-			ImGui::Unindent(35.0f);
-		}
-
-
-		ImGui::SetCursorPosX(ImGui::GetCursorPosX() - 3);
-		ImGui::Checkbox("##SceneExplorerDirectionalVisible", &m_scene.lighting.directional_light.is_active);
-
-		ImGui::SameLine();
-
-		if (ImGui::CollapsingHeader("Directional light##SceneExplorerDirectionalLighting")) {
-			ImGui::Indent(35.0f);
-
-			if (!m_scene.lighting.directional_light.is_active)
-				ImGui::BeginDisabled();
-
-			m_scene.lighting.directional_light.build_gui();
-
-			if (!m_scene.lighting.directional_light.is_active)
-				ImGui::EndDisabled();
-
-			ImGui::Unindent(35.0f);
-		}*/
 	}
 
 	ImGui::SetNextItemOpen(true, ImGuiCond_Once);
-	if (ImGui::CollapsingHeader("Models##SceneExplorerModels")) {
-		/*for (auto& kvp : m_scene.models) {
-			ds_basic_render_model& rmodel = kvp.second;
+	if (ImGui::CollapsingHeader("Objects##Objects")) {
+		ImGui::Indent();
 
-			std::string guid_str = rmodel.guid.to_string();
-			ImGui::PushID(guid_str.c_str());
+		if (m_systems->scene.current_scene) {
+			for (auto& kvp : m_systems->scene.current_scene->models) {
+				dv_scene_model& smodel = kvp.second;
 
-			ImGui::SetCursorPosX(ImGui::GetCursorPosX() - 3);
-			ImGui::Checkbox("##Visible", &rmodel.visible);
+				ImGui::PushID(DV_FORMAT_C("{}", smodel.uuid));
 
-			ImGui::SameLine();
-
-			if (ImGui::CollapsingHeader(FORMAT_C("{}", rmodel.name), &rmodel.flag_alive)) {
-				ImGui::Indent(35.0f);
-
-				if (!rmodel.visible)
-					ImGui::BeginDisabled();
-
-				ImGui::Text(rmodel.guid.to_string().c_str());
-
-				ImGui::SetCursorPosX(ImGui::GetCursorPosX() - 3);
-				ImGui::Checkbox("##Wireframe", &rmodel.wireframe);
-
-				ImGui::SameLine();
-				ImGui::Text("Wireframe");
-
-				ImGui::SetNextItemOpen(true, ImGuiCond_Once);
-				if (ImGui::CollapsingHeader("Transform")) {
-					ImGui::Indent(10.0f);
-
-					ImGui::Text("Translation");
-					ImGui::SetNextItemWidth(ImGui::GetContentRegionMax().x - ImGui::GetCursorPosX() + 3);
-					ImGui::DragFloat3("##Translate",
-									  (float*)&rmodel.transform.translation, 0.1f);
-
-
-					ImGui::Text("Rotation");
-					ImGui::SetNextItemWidth(ImGui::GetContentRegionMax().x - ImGui::GetCursorPosX() + 3);
-					ImGui::DragFloat3("##Rotate",
-									  (float*)&rmodel.transform.rotation, 1.0f, -360.0f, 360.0f);
-
-					ImGui::Text("Scale");
-					ImGui::SetNextItemWidth(ImGui::GetContentRegionMax().x - ImGui::GetCursorPosX() + 3);
-					ImGui::DragFloat3("##Scale",
-									  (float*)&rmodel.transform.scale, 0.1f, 0.1f, 10.0f);
-
-					ImGui::Unindent();
+				if (ImGui::Selectable(DV_FORMAT_C("{}##Object", smodel.name),
+					m_components->selected.in_selected(smodel)))
+				{
+					m_components->selected.select(smodel);
 				}
 
-				if (!rmodel.visible)
-					ImGui::EndDisabled();
-
-				ImGui::Unindent();
+				ImGui::PopID();
 			}
+		}
 
-			ImGui::PopID();
-		}*/
+		ImGui::Unindent();
 	}
 
 	ImGui::PopID();
