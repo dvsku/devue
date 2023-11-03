cf2ada484f47e510d521c4cc1b3a7097280e4262
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-03 03:18:01 +0100

    Start of material system
    - TEMP: just to get rendering system up and running

--- include/devue_core/systems/dv_sys_material.hpp
+++ include/devue_core/systems/dv_sys_material.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
+#include "models/pdo/dv_model.hpp"
+
 namespace devue::core {
 	class dv_sys_material {
+	public:
 
+	public:
+		void prepare_model_materials(dv_model& model);
 	};
 }
--- src/devue_core/src/systems/dv_sys_material.cpp
+++ src/devue_core/src/systems/dv_sys_material.cpp
@@ -1,1 +1,7 @@
 #include "systems/dv_sys_material.hpp"
+
+using namespace devue::core;
+
+void dv_sys_material::prepare_model_materials(dv_model& model) {
+
+}
