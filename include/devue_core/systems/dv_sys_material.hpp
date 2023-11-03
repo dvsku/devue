e3ae87502eb706387440157c42619dcfc6451ddc
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-03 04:20:23 +0100

    Get material from material system

--- include/devue_core/systems/dv_sys_material.hpp
+++ include/devue_core/systems/dv_sys_material.hpp
@@ -1,12 +1,21 @@
 #pragma once
 
 #include "models/pdo/dv_model.hpp"
+#include "scene/model/dv_scene_material.hpp"
+#include "utilities/dv_util_uuid.hpp"
+
+#include <unordered_map>
 
 namespace devue::core {
 	class dv_sys_material {
 	public:
 
 	public:
+		const dv_scene_material* get_material(devue::uuid uuid);
+
 		void prepare_model_materials(dv_model& model);
+
+	private:
+		std::unordered_map<devue::uuid, dv_scene_material> m_materials;
 	};
 }
--- src/devue_core/src/systems/dv_sys_material.cpp
+++ src/devue_core/src/systems/dv_sys_material.cpp
@@ -2,6 +2,11 @@
 
 using namespace devue::core;
 
+const dv_scene_material* dv_sys_material::get_material(devue::uuid uuid) {
+	if (!m_materials.contains(uuid)) return nullptr;
+	return &m_materials[uuid];
+}
+
 void dv_sys_material::prepare_model_materials(dv_model& model) {
 
 }
