0bf1b80a9b7c2ca586245fe11f00dc4a262504cc
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-03 16:00:04 +0100

    Prepare model material

--- include/devue_core/systems/dv_sys_material.hpp
+++ include/devue_core/systems/dv_sys_material.hpp
@@ -9,13 +9,12 @@
 namespace devue::core {
 	class dv_sys_material {
 	public:
-
-	public:
 		const dv_scene_material* get_material(devue::uuid uuid);
+		size_t count() const;
 
 		void prepare_model_materials(dv_model& model);
 
 	private:
-		std::unordered_map<devue::uuid, dv_scene_material> m_materials;
+		std::unordered_map<devue::uuid, std::pair<uint16_t, dv_scene_material>> m_materials;
 	};
 }
--- src/devue_core/src/systems/dv_sys_material.cpp
+++ src/devue_core/src/systems/dv_sys_material.cpp
@@ -2,11 +2,41 @@
 
 using namespace devue::core;
 
+dv_scene_material create_scene_material(dv_model& model, dv_material& material);
+
 const dv_scene_material* dv_sys_material::get_material(devue::uuid uuid) {
 	if (!m_materials.contains(uuid)) return nullptr;
-	return &m_materials[uuid];
+	return &m_materials[uuid].second;
+}
+
+size_t dv_sys_material::count() const {
+	return m_materials.size();
 }
 
 void dv_sys_material::prepare_model_materials(dv_model& model) {
+	for (auto& [uuid, material] : model.materials) {
+		// Increase ref count and continue
+		if (m_materials.contains(uuid)) {
+			m_materials[uuid].first++;
+			continue;
+		}
 
+		try {
+			m_materials[uuid] = {
+				1U,
+				create_scene_material(model, material)
+			};
+		}
+		catch (...) {
+			continue;
+		}
+	}
+}
+
+dv_scene_material create_scene_material(dv_model& model, dv_material& material) {
+	dv_scene_material scene_material;
+
+	// TODO: Create textures
+
+	return scene_material;
 }
