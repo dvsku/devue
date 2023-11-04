8804a7ecb37759e7f093034208de294e055f0938
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-04 16:18:19 +0100

    Implement texture system

--- include/devue_core/systems/dv_sys_texture.hpp
+++ include/devue_core/systems/dv_sys_texture.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "models/pdo/dv_model.hpp"
+#include "scene/model/dv_scene_material.hpp"
 #include "scene/model/dv_scene_texture.hpp"
+#include "importers/dv_texture_importer.hpp"
 #include "utilities/dv_util_uuid.hpp"
 
 #include <utility>
@@ -9,10 +12,23 @@
 namespace devue::core {
     class dv_sys_texture {
     public:
+        dv_sys_texture()                      = default;
+        dv_sys_texture(const dv_sys_texture&) = delete;
+        dv_sys_texture(dv_sys_texture&&)      = default;
 
+        dv_sys_texture& operator=(const dv_sys_texture&) = delete;
+        dv_sys_texture& operator=(dv_sys_texture&&)      = default;
+
+        bool prepare();
+
+        const dv_scene_texture* get_texture(devue::uuid uuid);
     	size_t count() const;
 
-    public:
+        void prepare_material_textures(dv_model& model, dv_material& material, dv_scene_material& smaterial);
+        void release_textures(dv_scene_material& smaterial);
+
+    private:
+        std::vector<dv_texture_importer> m_importers;
     	std::unordered_map<devue::uuid, std::pair<uint16_t, dv_scene_texture>> m_textures;
     };
 }
--- src/devue_core/src/systems/dv_sys_texture.cpp
+++ src/devue_core/src/systems/dv_sys_texture.cpp
@@ -1,2 +1,131 @@
 #include "systems/dv_sys_texture.hpp"
+#include "exceptions/dv_exception.hpp"
+#include "utilities/dv_util_string.hpp"
+#include "utilities/dv_util_log.hpp"
+#include "glad/glad.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/include/stb_image.h"
+
+#include <filesystem>
+
+using namespace devue::core;
+using namespace devue::shared;
+
+static dv_scene_texture create_scene_texture(std::filesystem::path& filepath, std::vector<dv_texture_importer>& importers);
+
+static dv_plugin_texture _import_stb_supported(const std::filesystem::path& path);
+
+///////////////////////////////////////////////////////////////////////////////
+// PUBLIC
+
+bool dv_sys_texture::prepare() {
+    m_importers.push_back({
+        {
+            {"Jpeg", ".jpg"},
+            {"PNG",  ".png"},
+        },
+        _import_stb_supported
+    });
+    
+    return true;
+}
+
+const dv_scene_texture* dv_sys_texture::get_texture(devue::uuid uuid) {
+    if (!m_textures.contains(uuid)) return nullptr;
+    return &m_textures[uuid].second;
+}
+
+size_t dv_sys_texture::count() const {
+    return m_textures.size();
+}
+
+void dv_sys_texture::prepare_material_textures(dv_model& model, 
+                                               dv_material& material, dv_scene_material& smaterial) 
+{
+    {
+        std::filesystem::path texture = std::filesystem::path(model.texture_dir).append(material.diffuse_texture);
+        devue::uuid uuid = dv_util_uuid::create(texture.string());
+
+        try {
+            if (m_textures.contains(uuid)) {
+                m_textures[uuid].first++;
+            }
+            else {
+                m_textures[uuid] = {
+                    1U,
+                    create_scene_texture(texture, m_importers)
+                };
+            }
+
+            smaterial.diffuse_texture_uuid = uuid;
+        }
+        catch(const dv_exception& e) {
+            DV_LOG("Failed to create `{}` diffuse texture `{}`. | {}", model.name, material.diffuse_texture, e.what());
+        }
+        catch (const std::exception& e) {
+            DV_LOG("Failed to create `{}` diffuse texture `{}`. | {}", model.name, material.diffuse_texture, e.what());
+        }
+    }
+}
+
+dv_scene_texture create_scene_texture(std::filesystem::path& filepath, std::vector<dv_texture_importer>& importers) {
+    if (!std::filesystem::exists(filepath))
+        throw;
+
+    std::string ext = filepath.extension().string();
+
+    auto cmp_fn = [&](const dv_file_type& type) {
+        return dv_util_string::contains(type.extensions, ext);
+    };
+
+    for (auto& importer : importers) {
+        if (std::none_of(importer.types.begin(), importer.types.end(), cmp_fn))
+            continue;
+
+        dv_plugin_texture ptexture = importer.fn(filepath.string());
+
+        if (!ptexture.height || !ptexture.width || ptexture.data.empty()) 
+            throw;
+
+        dv_scene_texture stexture;
+
+        glGenTextures(1, &stexture.texture_id);
+        glBindTexture(GL_TEXTURE_2D, stexture.texture_id);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_GENERATE_MIPMAP, GL_FALSE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, ptexture.width, ptexture.height, 
+                     0, GL_RGB, GL_UNSIGNED_BYTE, ptexture.data.data());
+
+        return stexture;
+    }
+
+    throw DV_EXCEPTION("No suitable importer found.");
+}
+
+dv_plugin_texture _import_stb_supported(const std::filesystem::path& path) {
+    dv_plugin_texture ptexture;
+    
+    stbi_set_flip_vertically_on_load(true);
+
+    int channels = 0;
+    unsigned char* data = stbi_load(path.string().c_str(), &ptexture.width, &ptexture.height, &channels, 0);
+
+    if (!data) {
+        stbi_image_free(data);
+        throw;
+    }
+
+    size_t size = (size_t)ptexture.width * (size_t)ptexture.height * (size_t)channels;
+    ptexture.data.resize(size);
+    ptexture.data.insert(ptexture.data.begin(), data, data + size);
+
+    stbi_image_free(data);
+
+    return ptexture;
+}
