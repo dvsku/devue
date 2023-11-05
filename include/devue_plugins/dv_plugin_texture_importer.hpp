9ce60db0d1fc7c02898f423dbc4ae1b4292da1b2
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-05 16:10:04 +0100

    Update texture importer to provide common plugin info

--- include/devue_plugins/dv_plugin_texture_importer.hpp
+++ include/devue_plugins/dv_plugin_texture_importer.hpp
@@ -5,10 +5,29 @@
 namespace devue::plugins {
     class dv_plugin_texture_importer {
     public:
+        struct serialized {
+            uint64_t size = 0U;
+            uint8_t* data = nullptr;
+        };
+
+        ///////////////////////////////////////////////////////////////////////
+        // PLUGIN DETAILS
+
+        virtual serialized get_name()   = 0;
+        virtual serialized get_author() = 0;
+        virtual serialized get_link()   = 0;
+
+        virtual uint8_t get_plugin_version_major() = 0;
+        virtual float get_plugin_version_minor()   = 0;
+        virtual uint32_t get_internal_version()    = 0;
+
+        ///////////////////////////////////////////////////////////////////////
+        // PLUGIN IMPL
+
         virtual void init()    = 0;
         virtual void cleanup() = 0;
 
-        virtual uint32_t texture_version() = 0;
-        virtual uint32_t plugin_version()  = 0;
+        virtual serialized get_suported_types()         = 0;
+        virtual serialized import(const char* filepath) = 0;
     };
 }
