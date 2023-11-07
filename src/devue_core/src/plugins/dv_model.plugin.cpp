b74932b07517b21139af4a360e79204ec6d48676
Nikola Stojšin <nikola.stojsin@hotmail.com>
2023-11-07 17:24:17 +0100

    Implement model plugin import

--- src/devue_core/src/plugins/dv_model.plugin.cpp
+++ src/devue_core/src/plugins/dv_model.plugin.cpp
@@ -13,6 +13,40 @@ dv_plugin_model dv_model_plugin::import(const std::string & filepath) {
     if (!serialized)
         throw;
 
+    dv_bin_reader br(serialized.data, serialized.size);
+    size_t meshes = 0U;
+
+    br >> meshes;
+    for (size_t i = 0; i < meshes; i++) {
+        dv_plugin_mesh mesh;
+
+        br >> mesh.name;
+        br >> mesh.material.name >> mesh.material.diffuse_texture;
+
+        size_t vertices = 0U;
+        br >> vertices;
+        for (size_t j = 0; j < vertices; j++) {
+            dv_plugin_vertex vertex{};
+
+            br >> vertex.position.x >> vertex.position.y >> vertex.position.z;
+            br >> vertex.normal.x   >> vertex.normal.y   >> vertex.normal.z;
+            br >> vertex.uv.x       >> vertex.uv.y;
+
+            mesh.vertices.push_back(vertex);
+        }
+
+        size_t indices = 0U;
+        br >> indices;
+        for (size_t j = 0; j < indices; j++) {
+            uint16_t index = 0U;
+            br >> index;
+
+            mesh.indices.push_back(index);
+        }
+
+        model.meshes.push_back(mesh);
+    }
+
     m_importer->cleanup();
 
     return model;
