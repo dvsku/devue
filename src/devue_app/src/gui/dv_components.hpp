00dcacde31fc851ebb9beff573512a044405c471
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-01-24 01:25:29 +0100

    Remove dv_selected

--- src/devue_app/src/gui/dv_components.hpp
+++ src/devue_app/src/gui/dv_components.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "gui/base/dv_selected.hpp"
 #include "gui/components/dv_comp_assets.hpp"
 #include "gui/components/dv_comp_console.hpp"
 #include "gui/components/dv_comp_dockspace.hpp"
@@ -12,16 +11,14 @@
 
 namespace devue {
     struct dv_components {
-    	dv_comp_assets		assets;
-    	dv_comp_console		console;
-    	dv_comp_dockspace	dockspace;
-    	dv_comp_hierarchy	hierarchy;
-    	dv_comp_properties	properties;
-    	dv_comp_scene		scene;
+    	dv_comp_assets	   assets;
+    	dv_comp_console	   console;
+    	dv_comp_dockspace  dockspace;
+    	dv_comp_hierarchy  hierarchy;
+    	dv_comp_properties properties;
+    	dv_comp_scene	   scene;
 
         dv_modal_import modal_import;
-
-    	dv_selected selected{};
 
     	dv_components() = delete;
     	dv_components(dv_systems* systems);
