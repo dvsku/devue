9a8c52a7c7862544ec02d5716c654cdfd9005bbf
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-01-26 19:20:21 +0100

    Rework plugin interface

--- include/devue_plugins/dv_plugin_importer.hpp
+++ include/devue_plugins/dv_plugin_importer.hpp
@@ -7,31 +7,22 @@ namespace devue::plugins {
     public:
         struct serialized {
             uint64_t size = 0U;
-            uint8_t* data = nullptr;
+            char*    data = nullptr;
 
             explicit operator bool() const {
                 return size && data;
             }
         };
 
-        ///////////////////////////////////////////////////////////////////////
-        // PLUGIN DETAILS
+        enum class plugin_type : uint8_t {
+            undefined = 0,
+            model     = 1,
+            texture   = 2
+        };
 
-        virtual serialized get_name()   = 0;
-        virtual serialized get_author() = 0;
-        virtual serialized get_link()   = 0;
-
-        virtual uint8_t get_plugin_version_major() = 0;
-        virtual float get_plugin_version_minor()   = 0;
-        virtual uint32_t get_internal_version()    = 0;
-
-        ///////////////////////////////////////////////////////////////////////
-        // PLUGIN IMPL
-
-        virtual void init()    = 0;
-        virtual void cleanup() = 0;
-
-        virtual serialized get_suported_types()         = 0;
-        virtual serialized import(const char* filepath) = 0;
+    public:
+        virtual serialized get_plugin_info()            noexcept = 0;
+        virtual serialized import(const char* filepath) noexcept = 0;
+        virtual void cleanup() noexcept = 0;
     };
 }
