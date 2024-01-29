80f5b0cdb3a21dbf92289e4572638e463a9e9308
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-01-29 13:39:59 +0100

    Update bounding box calc

--- src/devue_core/src/systems/dv_sys_model.cpp
+++ src/devue_core/src/systems/dv_sys_model.cpp
@@ -171,15 +171,13 @@ void calculate_bounding_box(dv_model& model) {
     model.bounding_box.minimum = { FLT_MAX, FLT_MAX, FLT_MAX };
     model.bounding_box.maximum = { std::numeric_limits<double>::lowest(), std::numeric_limits<double>::lowest(), std::numeric_limits<double>::lowest() };
 
-    for (const auto& [mesh_id, mesh] : model.meshes) {
-        for (const auto& vertex : mesh.vertices) {
-            model.bounding_box.minimum.x = min(model.bounding_box.minimum.x, vertex.position.x);
-            model.bounding_box.minimum.y = min(model.bounding_box.minimum.y, vertex.position.y);
-            model.bounding_box.minimum.z = min(model.bounding_box.minimum.z, vertex.position.z);
+    for (const auto& vertex : model.vertices) {
+        model.bounding_box.minimum.x = min(model.bounding_box.minimum.x, vertex.position.x);
+        model.bounding_box.minimum.y = min(model.bounding_box.minimum.y, vertex.position.y);
+        model.bounding_box.minimum.z = min(model.bounding_box.minimum.z, vertex.position.z);
 
-            model.bounding_box.maximum.x = max(model.bounding_box.maximum.x, vertex.position.x);
-            model.bounding_box.maximum.y = max(model.bounding_box.maximum.y, vertex.position.y);
-            model.bounding_box.maximum.z = max(model.bounding_box.maximum.z, vertex.position.z);
-        }
+        model.bounding_box.maximum.x = max(model.bounding_box.maximum.x, vertex.position.x);
+        model.bounding_box.maximum.y = max(model.bounding_box.maximum.y, vertex.position.y);
+        model.bounding_box.maximum.z = max(model.bounding_box.maximum.z, vertex.position.z);
     }
 }
