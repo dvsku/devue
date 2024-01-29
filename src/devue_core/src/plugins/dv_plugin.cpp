24c7033566d77042c267bdb3afd3e1665252782f
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-01-29 13:38:43 +0100

    Update plugin deserialization
    - conform to changes made to plugin core

--- src/devue_core/src/plugins/dv_plugin.cpp
+++ src/devue_core/src/plugins/dv_plugin.cpp
@@ -66,8 +66,53 @@ plugins::devue_plugin_model dv_plugin::import_model(const std::string& filepath)
 
     nlohmann::json json = nlohmann::json::from_cbor(serialized.data, serialized.data + serialized.size);
 
+    if (!json.contains("vertices") || json["vertices"].empty())
+        return plugins::devue_plugin_model();
+
     if (!json.contains("meshes") || json["meshes"].empty())
         return plugins::devue_plugin_model();
+
+    for (auto& json_vertex : json["vertices"]) {
+        plugins::devue_plugin_vertex vertex{};
+
+        if (json_vertex.contains("position")) {
+            auto& json_position = json_vertex["position"];
+
+            if (json_position.contains("x") && json_position["x"].is_number())
+                vertex.position.x = json_position["x"];
+
+            if (json_position.contains("y") && json_position["y"].is_number())
+                vertex.position.y = json_position["y"];
+
+            if (json_position.contains("z") && json_position["z"].is_number())
+                vertex.position.z = json_position["z"];
+        }
+
+        if (json_vertex.contains("normal")) {
+            auto& json_normal = json_vertex["normal"];
+
+            if (json_normal.contains("x") && json_normal["x"].is_number())
+                vertex.normal.x = json_normal["x"];
+
+            if (json_normal.contains("y") && json_normal["y"].is_number())
+                vertex.normal.y = json_normal["y"];
+
+            if (json_normal.contains("z") && json_normal["z"].is_number())
+                vertex.normal.z = json_normal["z"];
+        }
+
+        if (json_vertex.contains("uv")) {
+            auto& json_uv = json_vertex["uv"];
+
+            if (json_uv.contains("x") && json_uv["x"].is_number())
+                vertex.uv.x = json_uv["x"];
+
+            if (json_uv.contains("y") && json_uv["y"].is_number())
+                vertex.uv.y = json_uv["y"];
+        }
+
+        model.vertices.push_back(vertex);
+    }
 
     for (auto& json_mesh : json["meshes"]) {
         plugins::devue_plugin_mesh mesh;
@@ -85,58 +130,12 @@ plugins::devue_plugin_model dv_plugin::import_model(const std::string& filepath)
                 mesh.material.diffuse_texture = json_material["diffuse_texture"];
         }
 
-        if (json_mesh.contains("vertices") && json_mesh["vertices"].is_array()) {
-            auto& json_vertices = json_mesh["vertices"];
+        if (json_mesh.contains("indices") && json_mesh["indices"].is_array()) {
+            auto& json_indices = json_mesh["indices"];
 
-            for (auto& json_vertex : json_vertices) {
-                plugins::devue_plugin_vertex vertex{};
-
-                if (json_vertex.contains("position")) {
-                    auto& json_position = json_vertex["position"];
-
-                    if (json_position.contains("x") && json_position["x"].is_number())
-                        vertex.position.x = json_position["x"];
-
-                    if (json_position.contains("y") && json_position["y"].is_number())
-                        vertex.position.y = json_position["y"];
-
-                    if (json_position.contains("z") && json_position["z"].is_number())
-                        vertex.position.z = json_position["z"];
-                }
-
-                if (json_vertex.contains("normal")) {
-                    auto& json_normal = json_vertex["normal"];
-
-                    if (json_normal.contains("x") && json_normal["x"].is_number())
-                        vertex.normal.x = json_normal["x"];
-
-                    if (json_normal.contains("y") && json_normal["y"].is_number())
-                        vertex.normal.y = json_normal["y"];
-
-                    if (json_normal.contains("z") && json_normal["z"].is_number())
-                        vertex.normal.z = json_normal["z"];
-                }
-
-                if (json_vertex.contains("uv")) {
-                    auto& json_uv = json_vertex["uv"];
-
-                    if (json_uv.contains("x") && json_uv["x"].is_number())
-                        vertex.uv.x = json_uv["x"];
-
-                    if (json_uv.contains("y") && json_uv["y"].is_number())
-                        vertex.uv.y = json_uv["y"];
-                }
-
-                mesh.vertices.push_back(vertex);
-            }
-
-            if (json_mesh.contains("indices") && json_mesh["indices"].is_array()) {
-                auto& json_indices = json_mesh["indices"];
-
-                for (auto& json_index : json_indices) {
-                    if (json_index.is_number())
-                        mesh.indices.push_back(json_index);
-                }
+            for (auto& json_index : json_indices) {
+                if (json_index.is_number())
+                    mesh.indices.push_back(json_index);
             }
         }
 
