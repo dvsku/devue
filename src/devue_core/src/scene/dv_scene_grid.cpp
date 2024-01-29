c6b88fc2af7f42c692df3a7b2605901722022ad2
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-01-29 13:37:40 +0100

    Update grid model

--- src/devue_core/src/scene/dv_scene_grid.cpp
+++ src/devue_core/src/scene/dv_scene_grid.cpp
@@ -8,20 +8,17 @@ dv_scene_grid::dv_scene_grid() {
 
     dv_vertex v1, v2, v3, v4;
     v1.position = glm::vec3(-1.0f, -1.0f, 0.0f);
-    v2.position = glm::vec3(1.0f, -1.0f, 0.0f);
-    v3.position = glm::vec3(1.0f, 1.0f, 0.0f);
-    v4.position = glm::vec3(-1.0f, 1.0f, 0.0f);
+    v2.position = glm::vec3( 1.0f, -1.0f, 0.0f);
+    v3.position = glm::vec3( 1.0f,  1.0f, 0.0f);
+    v4.position = glm::vec3(-1.0f,  1.0f, 0.0f);
 
-    mesh.vertices.push_back(v1);
-    mesh.vertices.push_back(v2);
-    mesh.vertices.push_back(v3);
-    mesh.vertices.push_back(v4);
+    model.vertices.push_back(v1);
+    model.vertices.push_back(v2);
+    model.vertices.push_back(v3);
+    model.vertices.push_back(v4);
 
     mesh.faces.push_back({ 0, 1, 2 });
     mesh.faces.push_back({ 2, 3, 0 });
 
     model.meshes[dv_util_uuid::create("grid")] = mesh;
-
-    dv_scene_mesh rmesh;
-    meshes.push_back(rmesh);
 }
