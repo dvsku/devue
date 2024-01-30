47062bb9f2f0c2072f18bcde9878e2d670603364
Nikola Stojšin <nikola.stojsin@hotmail.com>
2024-01-30 08:20:03 +0100

    Exception throwing util

--- include/devue_core/exceptions/dv_exception.hpp
+++ include/devue_core/exceptions/dv_exception.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include "utilities/dv_util_string.hpp"
+
 #include <stdexcept>
 
 namespace devue::core {
+    #define DV_THROW_EXCEPTION(fmt, ...)    \
+        throw devue::core::dv_exception(std::vformat(fmt, std::make_format_args(__VA_ARGS__)))
+
     class dv_exception : public std::runtime_error {
     public:
     	dv_exception() 
